c556d7e08db15d1f6f3f2145d6312da50e27635f
athulraj10 <athulraj165@gmail.com>
2023-08-11 19:28:57 +0530

    beforeCart changed line number 8

--- src/components/ProductList/CartButtons/BeforeCart.js
+++ src/components/ProductList/CartButtons/BeforeCart.js
@@ -4,14 +4,12 @@ import "./CartButtons.css";
 import { addToCart } from "../../../redux/cart";
 import { CartContext } from "../../../context/cart/cartContext";
 
-const BeforeCart = () => {
+const BeforeCart = ({ product }) => {
    const { addToCart } = useContext(CartContext);
-   const dispatch=useDispatch();
-   
 
    return (
       <div className="before-cart">
-         <button className="add-cart-button" onClick={()=> dispatch(addToCart())}>
+         <button className="add-cart-button" onClick={() => addToCart(product)}>
             Add to cart
          </button>
       </div>

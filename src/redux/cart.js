76d8ce0d650e243a2ee54b709ff6af631d56a12c
athulraj10 <athulraj165@gmail.com>
2023-08-11 21:01:19 +0530

    cart created and slice created and exported

---
+++ src/redux/cart.js
@@ -0,0 +1,18 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const INITIAL_STATE={
+    cartList:[],
+    cartCount:0,
+}
+
+const cartSlice=createSlice({
+    name:'cart',
+    initialState:INITIAL_STATE,
+    reducers:{
+        increment:(state)=>{state.cartCount=+1},
+        decrement:(state)=>{state.cartCount=-1},
+        addToCart:(state)=>{state.cartCount=1},
+    }
+})
+export const {increment,decrement,addToCart}=cartSlice.actions;
+export default cartSlice.reducer;

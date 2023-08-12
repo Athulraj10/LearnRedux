7f5f0ad63b382b9a42179a40c9ffe0f3eaa25004
athulraj10 <athulraj165@gmail.com>
2023-08-12 11:00:26 +0530

    before cart

--- src/components/ProductList/CartButtons/BeforeCart.js
+++ src/components/ProductList/CartButtons/BeforeCart.js
@@ -5,16 +5,15 @@ import { addToCart } from "../../../redux/cart";
 import { CartContext } from "../../../context/cart/cartContext";
 
 const BeforeCart = ({ product }) => {
-   const dispatch=useDispatch()
-
-
-   return (
-      <div className="before-cart">
-         <button className="add-cart-button" onClick={() => dispatch(product)}>
-            Add to cart
-         </button>
-      </div>
-   );
+  const dispatch = useDispatch();
+ 
+  return (
+    <div className="before-cart">
+      <button className="add-cart-button" onClick={() => dispatch(product)}>
+        Add to cart
+      </button>
+    </div>
+  );
 };
 
 export default BeforeCart;
